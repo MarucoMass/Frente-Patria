7b2d385656f086b10e3cd781e10383548f4ccb5f
Mario <mariomass23@gmail.com>
2022-03-22 22:31:47 -0300

    cambios, sw y manifest json

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const item = document.querySelectorAll('.item');
 const menu = document.querySelector('li .sub-container a');
 const subMenu = document.getElementsByClassName('submenu');
 
+//Service Worker    #################################
+//###################################################
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('Registro de SW exitoso', reg))
+      .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
+
 // header
 hamburguer.addEventListener('click', ()=>{
     hamburguer.classList.toggle('active');
